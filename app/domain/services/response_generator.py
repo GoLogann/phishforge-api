0e45b2f4e03f40d9b11687be01318e131dbc1399
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-30 13:28:54 -0300

    feat: modify prompt

--- app/domain/services/response_generator.py
+++ app/domain/services/response_generator.py
@@ -11,7 +11,7 @@ class ResponseGenerator:
     def __init__(self):
         self.llm = ChatOpenAI(
             model_name=settings.MODEL_NAME_LLM,
-            temperature=0,
+            temperature=0.6,
             api_key=settings.OPENAI_API_KEY
         )
 
@@ -20,30 +20,33 @@ class ResponseGenerator:
             template=(
                 "Você é um assistente de IA especializado em gerar exemplos de e-mails de phishing para fins educacionais. "
                 "Sua tarefa é criar exemplos realistas, de e-mails de phishing com base no contexto fornecido. "
-                "Cada exemplo deve seguir a estrutura de um e-mail de phishing.\n\n"
+                "Cada exemplo deve seguir a estrutura de um e-mail de phishing. Sempre crie nomes reais para o remetente "
+                "e o receptor. É crie exemplos muito realistas. Seja criativo no conteudo do e-mail.\n\n"
                 "Contexto: {context}\n\n"
+                "Documentos Relavantes para incrementar sua resposta: {relevant_docs}\n\n"
                 "Diculdade: {difficulty}\n\n"
                 "Gere um exemplo de e-mail de phishing com base no contexto e na pergunta. "
                 "Forneça a resposta no seguinte formato JSON:\n"
                 
                 "{{\n"
-                "  \"receptor\": \"email@example.com\",\n"
-                "  \"remetente\": \"fake_sender@example.com\",\n"
-                "  \"assunto\": \"Urgente: Verifique sua conta\",\n"
-                "  \"conteudo\": \"Caro usuário, por favor, verifique sua conta clicando no link abaixo...\",\n"
-                "  \"links\": [\"http://malicious-link.com\"]\n"
+                "  \"receptor\": \"\",\n"
+                "  \"remetente\": \"\",\n"
+                "  \"assunto\": \"\",\n"
+                "  \"conteudo\": \"\",\n"
+                "  \"links\": [\"\"]\n"
                 "}}\n\n"
             )
         )
 
         self.chain = self.prompt_template | self.llm
 
-    async def generate_response(self, difficulty: str, context: str):
+    async def generate_response(self, difficulty: str, context: str, relevant_docs):
         try:
             response = self.chain.invoke(
                 {
                     "context": context,
                     "difficulty": difficulty,
+                    "relevant_docs": relevant_docs
                 }
             )
 
