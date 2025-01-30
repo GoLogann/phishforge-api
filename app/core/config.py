71b6a4e36e2200f0d7339e267af78075b7d6ca61
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2025-01-30 12:24:07 -0300

    feat: add model OpenAI

--- app/core/config.py
+++ app/core/config.py
@@ -2,8 +2,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MODEL_NAME_EMBEDDING: str = "all-MiniLM-L6-v2"
-    MODEL_NAME_LLM: str = "deepseek-r1:7b"
+    MODEL_NAME_LLM: str = "gpt-4o"
     QDRANT_URL: str = "http://localhost:6333"
+    OPENAI_API_KEY: str = ""
     CHUNK_SIZE: int = 350
     CHUNK_OVERLAP: int = 20
     TOP_K_DOCUMENTS: int = 4
